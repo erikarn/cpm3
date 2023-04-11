3b100b3dea9856a06df26c7f0a0c9d8fba565f45
Adrian Chadd <adrian@FreeBSD.org>
2023-04-10 21:49:03 -0700

    more disassemble
    I'm finally getting my head around the XBIOS init + jumpblock area.

--- pcw/j15cpm3/dis.sh
+++ pcw/j15cpm3/dis.sh
@@ -6,8 +6,10 @@ SYMFILE="xbios-jumpblock-init.sym"
 # this needs to be per file being disassembled
 BLKFILE="xbios-jumpblock-init.blk"
 
+BLKFILE_XBIOS="xbios-4.blk"
+
 /home/adrian/local/bin/z80dasm -v -g 0x0080 -t -a -l -o xbios-jumpblock-init.txt -S ${SYMFILE} -b ${BLKFILE} xbios-jumpblock-init.bin
 
 BLKFILE="xbios-4.blk"
 
-/home/adrian/local/bin/z80dasm -g 0x3c62 -t -v -a -l -S ${SYMFILE} -b ${BLKFILE} -o xbios-4.txt xbios-4.bin
+/home/adrian/local/bin/z80dasm -g 0x3c62 -t -v -a -l -S ${SYMFILE} -b ${BLKFILE_XBIOS} -o xbios-4.txt xbios-4.bin
