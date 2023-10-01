4c95968ccc2b5a6cc60d7103c993d1e159993bfc
Adrian Chadd <adrian@FreeBSD.org>
2023-09-30 21:38:20 -0700

    [j15cpm3] build the non-reversed .bin file contents as well.

--- pcw/j15cpm3/bin/build.sh
+++ pcw/j15cpm3/bin/build.sh
@@ -4,4 +4,8 @@
 # git repository.
 
 ../../tools/disassemble ../orig/J15CPM3.EMT ../orig/file-memmap.txt
+../../tools/rev bdos-banked-rev.bin bdos-banked.bin
+../../tools/rev bdos-resident-rev.bin bdos-resident.bin
+../../tools/rev bios-banked-rev.bin bios-banked.bin
+../../tools/rev bios-resident-rev.bin bios-resident.bin
 
