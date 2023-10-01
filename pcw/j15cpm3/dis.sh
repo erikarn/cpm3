19bbbc65dc01f6661e715d9458fb7ee146a87a83
Adrian Chadd <adrian@FreeBSD.org>
2023-09-30 21:41:45 -0700

    [j15cpm3] update paths in dis.sh

--- pcw/j15cpm3/dis.sh
+++ pcw/j15cpm3/dis.sh
@@ -8,8 +8,8 @@ BLKFILE="xbios-jumpblock-init.blk"
 
 BLKFILE_XBIOS="xbios-4.blk"
 
-/home/adrian/local/bin/z80dasm -v -g 0x0080 -t -a -l -o xbios-jumpblock-init.txt -S ${SYMFILE} -b ${BLKFILE} xbios-jumpblock-init.bin
+/home/adrian/local/bin/z80dasm -v -g 0x0080 -t -a -l -o xbios-jumpblock-init.txt -S ${SYMFILE} -b ${BLKFILE} bin/xbios-jumpblock-init.bin
 
 BLKFILE="xbios-4.blk"
 
-/home/adrian/local/bin/z80dasm -g 0x3c62 -t -v -a -l -S ${SYMFILE} -b ${BLKFILE_XBIOS} -o xbios-4.txt xbios-4.bin
+/home/adrian/local/bin/z80dasm -g 0x3c62 -t -v -a -l -S ${SYMFILE} -b ${BLKFILE_XBIOS} -o xbios-4.txt bin/xbios-4.bin
